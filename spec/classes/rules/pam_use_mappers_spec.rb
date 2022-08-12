80a33543d5b346a4ec2b0689cf30e48eeacefd22
Thomas Krieger <tom@tom-krieger.de>
2022-08-12 13:18:34 +0200

    create missing conf file

--- spec/classes/rules/pam_use_mappers_spec.rb
+++ spec/classes/rules/pam_use_mappers_spec.rb
@@ -19,6 +19,14 @@ describe 'cis_security_hardening::rules::pam_use_mappers' do
           is_expected.to compile
 
           if enforce
+            is_expected.to contain_file('/etc/pam_pkcs11/pam_pkcs11.conf')
+                .with(
+                  'ensure' => 'file',
+                  'owner'  => 'root',
+                  'group'  => 'root',
+                  'mode'   => '0644',
+                )
+            
             is_expected.to contain_file_line('pam use mappers')
               .with(
                 'ensure' => 'present',
@@ -26,6 +34,7 @@ describe 'cis_security_hardening::rules::pam_use_mappers' do
                 'line'   => 'use_mappers = pwent',
                 'match'  => '^#?use_mappers',
               )
+              .that_requires('File[/etc/pam_pkcs11/pam_pkcs11.conf]')
           end
         }
       end
