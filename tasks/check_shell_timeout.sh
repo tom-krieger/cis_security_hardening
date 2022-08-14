755c93fb06f919380b427315361e95a7f50f609a
Thomas Krieger <tom@tom-krieger.de>
2022-08-14 10:13:36 +0200

    use variable for timoeout

--- tasks/check_shell_timeout.sh
+++ tasks/check_shell_timeout.sh
@@ -6,10 +6,10 @@ output2=""
 [ -f /etc/bash.bashrc ] && BRC="/etc/bash.bashrc"
 
 for f in "$BRC" /etc/profile /etc/profile.d/*.sh ; do
-    grep -Pq '\bTMOUT=(600|[1-5][0-9][0-9]|[1-9][0-9]|[1-9])\b' "$f" && grep -Pq '\breadonly\h+TMOUT(\h+|\h*;|\h*$|=(600|[1-5][0-9][0-9]|[1-9][0-9]|[1-9]))\b' "$f" && grep -Pq '\bexport\h+([^#\n\r]+\h+)?TMOUT\b' "$f" && output1="$f"
+    grep -Pq "\bTMOUT=(${PT_tmout}|[1-5][0-9][0-9]|[1-9][0-9]|[1-9])\b" "$f" && grep -Pq "\breadonly\h+TMOUT(\h+|\h*;|\h*$|=(${PT_tmout}|[1-5][0-9][0-9]|[1-9][0-9]|[1-9]))\b" "$f" && grep -Pq '\bexport\h+([^#\n\r]+\h+)?TMOUT\b' "$f" && output1="$f"
 done
 
-output2=$(grep -Ps '\bTMOUT=(6[0-9][1-9]|[7-9][0-9][0-9]|[1-9]{3,}|0+)\b' /etc/profile /etc/profile.d/*.sh $BRC)
+output2=$(grep -Ps "\bTMOUT=(${PT_tmout:1:1}[0-9][1-9]|[7-9][0-9][0-9]|[1-9]{3,}|0+)\b" /etc/profile /etc/profile.d/*.sh $BRC)
 
 if [ -n "$output1" ] && [ -z "$output2" ]
 then
