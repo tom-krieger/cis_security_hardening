79e41910d00426cec1836c18982e34e838ea3cf4
Thomas Krieger <tom@tom-krieger.de>
2022-08-16 10:13:29 +0200

    sort rules

--- lib/facter/cis_security_hardening/utils/read_auditd_data.rb
+++ lib/facter/cis_security_hardening/utils/read_auditd_data.rb
@@ -7,7 +7,7 @@ def read_auditd_data
   cmd = "find /usr -xdev \\( -perm -4000 -o -perm -2000 \\) -type f | awk '{print \"-a always,exit -S all -F path=\" $1 \" -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged\"; }'"
   rules_raw = Facter::Core::Execution.exec(cmd).split("\n")
   priv_cmds.push(rules_raw)
-  auditd['priv-cmds-list'] = priv_cmds.uniq
+  auditd['priv-cmds-list'] = priv_cmds.uniq.sort
 
   cmd = 'find /etc/audit/ /etc/audit/rules.d -type f 2>/dev/null'
   conf_raw = Facter::Core::Execution.exec(cmd).split("\n")
