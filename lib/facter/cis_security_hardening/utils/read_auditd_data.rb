a70638ea0b02de952a35db8ed3ccf4a7a352874e
Thomas Krieger <tom@tom-krieger.de>
2022-08-16 11:00:57 +0200

    store only priv cmds not auditd rules

--- lib/facter/cis_security_hardening/utils/read_auditd_data.rb
+++ lib/facter/cis_security_hardening/utils/read_auditd_data.rb
@@ -4,9 +4,8 @@ def read_auditd_data
   auditd = {}
 
   priv_cmds = []
-  cmd = "find /usr -xdev \\( -perm -4000 -o -perm -2000 \\) -type f | awk '{print \"-a always,exit -S all -F path=\" $1 \" -F perm=x -F auid>=1000 -F auid!=-1 -F key=privileged\"; }'"
-  rules_raw = Facter::Core::Execution.exec(cmd).split("\n")
-  priv_cmds = rules_raw
+  cmd = "find /usr -xdev \\( -perm -4000 -o -perm -2000 \\) -type f"
+  priv_cmds = Facter::Core::Execution.exec(cmd).split("\n")
   auditd['priv-cmds-list'] = priv_cmds.uniq.sort
 
   cmd = 'find /etc/audit/ /etc/audit/rules.d -type f 2>/dev/null'
