9aaf0c9a9b054b94f416ec2b19123e96eba1e810
Thomas Krieger <tom@tom-krieger.de>
2023-08-05 19:19:19 +0200

    changed pam module ordering

--- manifests/rules/pam_lockout.pp
+++ manifests/rules/pam_lockout.pp
@@ -239,16 +239,18 @@ class cis_security_hardening::rules::pam_lockout (
             type      => 'auth',
             control   => 'sufficient',
             module    => 'pam_faillock.so',
+            position  => 'after *[type_"auth" and control="[default=die]"]',
             arguments => ['authsucc'],
             require   => Pam['pam-common-auth-require-faillock-die'],
           }
 
           Pam { 'pam-common-account-required-faillock':
-            ensure  => present,
-            service => 'common-account',
-            type    => 'account',
-            control => 'required',
-            module  => 'pam_faillock.so',
+            ensure   => present,
+            service  => 'common-account',
+            type     => 'account',
+            control  => 'required',
+            module   => 'pam_faillock.so',
+            position => 'last *[type="account"]',
           }
 
           file_line { 'faillock_fail_interval':
