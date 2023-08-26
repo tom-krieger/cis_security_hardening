a297fae2e20a59ecd3d7326c629339d66ea12343
Thomas Krieger <tom@tom-krieger.de>
2023-08-26 11:04:05 +0200

    fixed unit test

--- spec/classes/rules/systemd_journal_remote_receive_spec.rb
+++ spec/classes/rules/systemd_journal_remote_receive_spec.rb
@@ -32,7 +32,7 @@ describe 'cis_security_hardening::rules::systemd_journal_remote_receive' do
           is_expected.to contain_service('systemd-journal-remote.socket')
             .with(
               'ensure' => 'stopped',
-              'enable' => true,
+              'enable' => false,
             )
         else
           is_expected.not_to contain_service('systemd-journal-remote.socket')
