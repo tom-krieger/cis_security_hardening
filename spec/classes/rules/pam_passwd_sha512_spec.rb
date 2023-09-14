3171182bef2d94cf7d312c4abd23db48b1e3b181
Thomas Krieger <tom@tom-krieger.de>
2023-09-14 19:40:56 +0200

    updted unut test and updated action

--- spec/classes/rules/pam_passwd_sha512_spec.rb
+++ spec/classes/rules/pam_passwd_sha512_spec.rb
@@ -95,6 +95,8 @@ describe 'cis_security_hardening::rules::pam_passwd_sha512' do
                     .that_notifies('Exec[authselect-apply-changes]')
                 end
 
+                is_expected.not_to contain_file_line('set crypt method')
+
               elsif os_facts[:os]['family'].casecmp('debian').zero?
                 is_expected.not_to contain_exec('update authselect config for sha512 system-auth')
                 is_expected.not_to contain_exec('update authselect config for sha512 password-auth')
