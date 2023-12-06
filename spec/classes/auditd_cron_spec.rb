750562307ca04af5a72c8f2c34717eb8710ec135
Kenyon Ralph <quic_kralph@quicinc.com>
2023-12-06 12:39:33 -0800

    allow for disabling the cron jobs

--- spec/classes/auditd_cron_spec.rb
+++ spec/classes/auditd_cron_spec.rb
@@ -6,41 +6,83 @@ describe 'cis_security_hardening::auditd_cron' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do
-        {
-          'dirs_to_include' => ['/usr'],
-          'start_time_minute' => 40,
-          'start_time_hour' => 4,
-          'cron_repeat' => '2',
-          'output_file' => '/usr/share/cis_security_hardening/data/auditd_priv_cmds.txt',
-          'script' => '/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh',
-        }
+
+      context 'using defaults' do
+        let(:params) do
+          {
+            'dirs_to_include' => ['/usr'],
+            'start_time_minute' => 40,
+            'start_time_hour' => 4,
+            'cron_repeat' => '2',
+            'output_file' => '/usr/share/cis_security_hardening/data/auditd_priv_cmds.txt',
+            'script' => '/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh',
+          }
+        end
+
+        it do
+          is_expected.to compile.with_all_deps
+
+          is_expected.to contain_file('/etc/cron.d/auditd_priv_commands.cron')
+            .with(
+              'ensure' => 'absent',
+            )
+
+          is_expected.to contain_file('/etc/cron.d/auditd_priv_commands')
+            .with(
+              'ensure'  => 'file',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0644',
+            )
+
+          is_expected.to contain_file('/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh')
+            .with(
+              'ensure'  => 'file',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0700',
+            )
+        end
       end
 
-      it {
-        is_expected.to compile
+      context 'absent' do
+        let(:params) do
+          {
+            'ensure' => 'absent',
+            'dirs_to_include' => ['/usr'],
+            'start_time_minute' => 40,
+            'start_time_hour' => 4,
+            'cron_repeat' => '2',
+            'output_file' => '/usr/share/cis_security_hardening/data/auditd_priv_cmds.txt',
+            'script' => '/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh',
+          }
+        end
 
-        is_expected.to contain_file('/etc/cron.d/auditd_priv_commands.cron')
-          .with(
-            'ensure'  => 'absent',
-          )
+        it do
+          is_expected.to compile.with_all_deps
 
-        is_expected.to contain_file('/etc/cron.d/auditd_priv_commands')
-          .with(
-            'ensure'  => 'file',
-            'owner'   => 'root',
-            'group'   => 'root',
-            'mode'    => '0644',
-          )
+          is_expected.to contain_file('/etc/cron.d/auditd_priv_commands.cron')
+            .with(
+              'ensure' => 'absent',
+            )
 
-        is_expected.to contain_file('/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh')
-          .with(
-            'ensure'  => 'file',
-            'owner'   => 'root',
-            'group'   => 'root',
-            'mode'    => '0700',
-          )
-      }
+          is_expected.to contain_file('/etc/cron.d/auditd_priv_commands')
+            .with(
+              'ensure'  => 'absent',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0644',
+            )
+
+          is_expected.to contain_file('/usr/share/cis_security_hardening/bin/auditd_priv_cmds.sh')
+            .with(
+              'ensure'  => 'absent',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0700',
+            )
+        end
+      end
     end
   end
 end
--- spec/classes/cis_security_hardening_spec.rb
+++ spec/classes/cis_security_hardening_spec.rb
@@ -9,43 +9,49 @@ describe 'cis_security_hardening' do
     postrun_options.each do |postrun|
       context "on #{os} with postrun = #{postrun}" do
         let(:facts) { os_facts }
-        let(:params) do
-          {
-            'level' => '2',
-            'update_postrun_command' => postrun,
-          }
+
+        context 'using defaults' do
+          let(:params) do
+            {
+              'level' => '2',
+              'update_postrun_command' => postrun,
+            }
+          end
+
+          it do
+            os_vers = if os_facts[:os]['name'].casecmp('ubuntu').zero?
+                        os_facts[:os]['release']['major'].split("/\./")
+                      else
+                        os_facts[:os]['release']['major']
+                      end
+
+            key = "cis_security_hardening::benchmark::#{os_facts[:os]['name']}::#{os_vers}"
+            # is_expected.to compile
+            is_expected.to compile.with_all_deps
+            is_expected.to contain_class('cis_security_hardening::services')
+            is_expected.to contain_class('cis_security_hardening::config')
+            is_expected.to contain_class('cis_security_hardening::auditd_cron')
+
+            unless os_facts[:os]['name'].casecmp('ubuntu').zero? ||
+                  os_facts[:os]['name'].casecmp('debian').zero? ||
+                  os_facts[:os]['name'].casecmp('centos').zero? ||
+                  os_facts[:os]['name'].casecmp('redhat').zero? ||
+                  os_facts[:os]['name'].casecmp('almalinux').zero? ||
+                  os_facts[:os]['name'].casecmp('rocky').zero? ||
+                  os_facts[:os]['name'].casecmp('sles').zero?
+
+              is_expected.to contain_echo('no bundles')
+                .with(
+                  'message'  => "No bundles found, enforcing nothing. (key = #{key})",
+                  'loglevel' => 'warning',
+                  'withpath' => false,
+                )
+            end
+          end
         end
 
-        it {
-          os_vers = if os_facts[:os]['name'].casecmp('ubuntu').zero?
-                      os_facts[:os]['release']['major'].split("/\./")
-                    else
-                      os_facts[:os]['release']['major']
-                    end
-
-          key = "cis_security_hardening::benchmark::#{os_facts[:os]['name']}::#{os_vers}"
-          # is_expected.to compile
-          is_expected.to compile.with_all_deps
-          is_expected.to contain_class('cis_security_hardening::services')
-          is_expected.to contain_class('cis_security_hardening::config')
-          is_expected.to contain_class('cis_security_hardening::auditd_cron')
-
-          unless os_facts[:os]['name'].casecmp('ubuntu').zero? ||
-                 os_facts[:os]['name'].casecmp('debian').zero? ||
-                 os_facts[:os]['name'].casecmp('centos').zero? ||
-                 os_facts[:os]['name'].casecmp('redhat').zero? ||
-                 os_facts[:os]['name'].casecmp('almalinux').zero? ||
-                 os_facts[:os]['name'].casecmp('rocky').zero? ||
-                 os_facts[:os]['name'].casecmp('sles').zero?
-
-            is_expected.to contain_echo('no bundles')
-              .with(
-                'message'  => "No bundles found, enforcing nothing. (key = #{key})",
-                'loglevel' => 'warning',
-                'withpath' => false,
-              )
-          end
-        }
+        context 'with cron jobs absent' do
+        end
       end
     end
   end
